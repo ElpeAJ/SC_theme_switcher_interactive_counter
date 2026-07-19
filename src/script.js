89eeb70944883b8ef48cb01579e362afbbe9bea4
ElpeAJ <elpediaarthurjunior@gmail.com>
2026-07-19 19:20:23 +0000

    Adding JS file with steps as comments

---
+++ src/script.js
@@ -0,0 +1,22 @@
+// STEP 1: Select and Link HTML elements to JavaScript variables
+
+
+// STEP 2: Create a variable to keep track of the application data (State)
+
+
+// STEP 3: Listen for a click on the Increase button 
+// & Create function to increase the count data by 1 
+// and update the HTML text on screen
+
+
+
+// STEP 4: Listen for a click on the Decrease button
+// & Create function to decrease the count data by 1 
+// and update the HTML text on screen
+
+
+
+// STEP 5: Listen for a click on the Dark Mode toggle button
+
+
+
